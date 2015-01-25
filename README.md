59e02890c33f030105426cda0b94079fcd4541ee
denormalize <denormalize@gmail.com>
2015-01-25 00:44:05 -0500

    Added first cut at serial motor control sketch

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # motor-control
-Arduino based USB Motor Control
+Arduino based USB Motor Control for Easy Stepper v4.4
+
+Allows you to send serial commands to an Arduino board to control a stepper motor using an Easy Stepper v4.4
+
+Accepts a single command over serial connection specifying four parameters in the following string pattern
+
+"[resolution],[direction],[steps],[step_delay]\n"
+
+resolution is an integer value that specifys the size of the step
+1 = full step
+2 = half step
+4 = quarter step
+8 = eigth step
+
+direction is an integer value that specifies the direction the motor will rotate
+0 = Clockwiae
+1 = Counter Clockwise
+
+steps is an integer value that specifies the number of steps to move the motor
+
+delay is an integer value that specifies the delay between each step
+
+
+
+Testing:  
+
+1. Load serial-motor-control sketch onto your arduino board
+
+2. Configure pin constants to match your Arduino / Easy Stepper v4.4 pin mappings for MS1, MS2, STEP and DIR
+
+3. install pySerial on your computer (sudo pip install pyserial)
+
+4. Run this short script to test motor movement, changing the serial address to the address of your Arduino board
+
+import serial
+ser = serial.Serial('/dev/tty.usbmodem14211', 115200)
+ser.write('8,1,1600,100\n')
+
+
+
---
+++ serial-motor-control/serial-motor-control.ino
@@ -0,0 +1,80 @@
+/* PIN CONSTANTS */
+#define MS1 8
+#define MS2 9
+#define STEP 12
+#define DIR 13
+
+/* DIRECTION CONSTANTS */
+#define CLOCKWISE 0
+#define COUNTER_CLOCKWISE 1
+
+int minDelay = 800;
+
+void setup() {             
+  pinMode(MS1, OUTPUT);
+  pinMode(MS2, OUTPUT);
+  pinMode(STEP, OUTPUT); 
+  pinMode(DIR, OUTPUT);
+  
+  digitalWrite(STEP, LOW);
+  
+  setResolution(8);
+  setDirection(CLOCKWISE);
+  
+  Serial.begin(115200);
+  Serial.println("Ready");
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    int res = Serial.parseInt();
+    int dir = Serial.parseInt();
+    int steps = Serial.parseInt();
+    int stepDelay = Serial.parseInt();
+    
+    if(Serial.read() == '\n'){
+      move(res, dir, steps, stepDelay);
+    }
+  }
+}
+void setResolution(int resolution){
+  switch(resolution){
+    case 1:
+      digitalWrite(MS1, LOW);
+      digitalWrite(MS2, LOW);
+      break;
+    case 2:
+      digitalWrite(MS1, HIGH); 
+      digitalWrite(MS2, LOW);
+      break;
+    case 4:
+      digitalWrite(MS1, LOW);
+      digitalWrite(MS2, HIGH);
+      break; 
+    case 8:
+      digitalWrite(MS1, HIGH); 
+      digitalWrite(MS2, HIGH);
+      break;
+  }
+}
+
+void setDirection(int motor_direction){
+  digitalWrite(DIR, (motor_direction) ? HIGH : LOW);
+}
+
+void move(int resolution, int motor_direction, int steps, int stepDelay){
+  setResolution(resolution);
+  setDirection(motor_direction);
+  
+  if(stepDelay < minDelay / resolution){
+    stepDelay = minDelay / resolution;
+  }
+  
+  for(int i=0; i < steps; i++){
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(minDelay/resolution);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(stepDelay);
+  }
+}
+
