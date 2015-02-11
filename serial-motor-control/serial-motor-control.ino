f984395bbf9cbfba989829971f02b193d3d89c19
btweller <btweller@gmail.com>
2015-02-10 22:10:34 -0600

    Working AccelStepper implementation with EasyDriver v4.4.

--- serial-motor-control/serial-motor-control.ino
+++ serial-motor-control/serial-motor-control.ino
@@ -1,3 +1,5 @@
+#include <AccelStepper.h>
+
 #include <QueueList.h>
 #include "structures.h"
 /* PIN CONSTANTS */
@@ -23,19 +25,20 @@
 QueueList <instruction> queue;
 instruction current;
 
+AccelStepper stepper(AccelStepper::DRIVER, STEP, DIR);
+
 void setup() {             
   pinMode(MS1, OUTPUT);
   pinMode(MS2, OUTPUT);
   pinMode(STEP, OUTPUT); 
   pinMode(DIR, OUTPUT);
   pinMode(SLP, OUTPUT);
-  
+
   digitalWrite(STEP, LOW);
-  digitalWrite(SLP, LOW); //Initialize Sleeping
+//  digitalWrite(SLP, LOW); //Initialize Sleeping
 
   setResolution(8);
-  setDirection(CLOCKWISE);
-  
+
   Serial.begin(115200);
   Serial.println("Ready");
 }
@@ -45,47 +48,44 @@ void loop() {
   run();
 }
 
-void checkSerial(){
-  if(Serial.available() > 0){
+void checkSerial() {
+  if (Serial.available() > 0) {
     wake(); //wake up and get ready
-    
+
     int res = Serial.parseInt();
     int dir = Serial.parseInt();
     int steps = Serial.parseInt();
     unsigned long stepDelay = Serial.parseFloat();
     int ref = Serial.parseInt();
-    
+
     int nonNumeric = Serial.read();
-    
-    if(nonNumeric == '\n'){
-      if(res == 0 && dir == 0 && steps == 0 && stepDelay == 0){
+
+    if (nonNumeric == '\n') {
+      if (res == 0 && dir == 0 && steps == 0 && stepDelay == 0) {
         stopMotor();
       }
-      else{
+      else {
         queueInstruction(res, dir, steps, stepDelay, ref);
       }
     }
   }
 }
 
-void stopMotor(){
+void stopMotor() {
   current.state = COMPLETE;
+  stepper.stop();
   Serial.print(current.ref);
   Serial.println(":canceled");
-  
-  while(!queue.isEmpty()){
+
+  while (!queue.isEmpty()) {
     instruction canceled = queue.pop();
     Serial.print(canceled.ref);
     Serial.println(":canceled");
   }
 }
 
-void queueInstruction(int res, int dir, int steps, unsigned long int stepDelay, int ref){
-  if(stepDelay / 2 < MINDELAY / res){
-    Serial.print(ref);
-    Serial.println(":delay too short");
-  }
-  else if(freeRam() < MINRAM){
+void queueInstruction(int res, int dir, int steps, unsigned long int stepDelay, int ref) {
+  if(freeRam() < MINRAM) {
     Serial.print(ref);
     Serial.println(":queue full");
   }
@@ -93,8 +93,8 @@ void queueInstruction(int res, int dir, int steps, unsigned long int stepDelay, 
     instruction incoming;
     incoming.res = res;
     incoming.dir = dir;
-    incoming.steps = steps * 2; //account for half step behavior
-    incoming.stepDelay = stepDelay / 2; //Split delay between High and LOW half steps
+    incoming.steps = steps;
+    incoming.stepDelay = stepDelay; // This is SPEED now.
     incoming.ref = ref;
     incoming.state = NEW;
     incoming.previousMicros = 0;
@@ -107,79 +107,89 @@ void queueInstruction(int res, int dir, int steps, unsigned long int stepDelay, 
   }
 }
 
-void setResolution(int resolution){
-  switch(resolution){
-    case 1:
-      digitalWrite(MS1, LOW);
-      digitalWrite(MS2, LOW);
-      break;
-    case 2:
-      digitalWrite(MS1, HIGH); 
-      digitalWrite(MS2, LOW);
-      break;
-    case 4:
-      digitalWrite(MS1, LOW);
-      digitalWrite(MS2, HIGH);
-      break; 
-    case 8:
-      digitalWrite(MS1, HIGH); 
-      digitalWrite(MS2, HIGH);
-      break;
+void setResolution(int resolution) {
+  switch(resolution) {
+  case 1:
+    digitalWrite(MS1, LOW);
+    digitalWrite(MS2, LOW);
+    break;
+  case 2:
+    digitalWrite(MS1, HIGH); 
+    digitalWrite(MS2, LOW);
+    break;
+  case 4:
+    digitalWrite(MS1, LOW);
+    digitalWrite(MS2, HIGH);
+    break; 
+  case 8:
+    digitalWrite(MS1, HIGH); 
+    digitalWrite(MS2, HIGH);
+    break;
   }
 }
 
 
-void sleep(){
-  digitalWrite(SLP, LOW); //Sleep
+void sleep() {
+//  digitalWrite(SLP, LOW); //Sleep
 }
 
-void wake(){
-  digitalWrite(SLP, HIGH); //WakeUp!
+void wake() {
+//  digitalWrite(SLP, HIGH); //WakeUp!
 }
 
-void setDirection(int dir){
-  digitalWrite(DIR, (dir) ? HIGH : LOW);
-}
+void run() {
+  if(current.state == NEW && current.steps != 0){
+    Serial.print(current.ref);
+    Serial.print(":NEW:res:");
+    Serial.print(current.res);
+    Serial.print(":steps:");
+    Serial.print(current.steps);
+    Serial.print(":dir:");
+    Serial.print(current.dir);
+    Serial.print(":speed:");
+    Serial.println(current.stepDelay);
 
-void halfStep(){
-  digitalWrite(STEP, (current.stepState) ? HIGH : LOW);
-  current.stepState = !current.stepState;
-}
-
-void run(){
-  if(current.state == NEW && current.steps > 0){
     setResolution(current.res);
-    setDirection(current.dir);
-    
-    halfStep();
-    
+
     current.state = PROCESSING;
-    current.previousMicros = micros();
-    current.steps--;
-  } 
-  
-  else if(current.state == PROCESSING && current.steps > 0){
-    unsigned long currentMicros = micros();
-    
-    if((unsigned long)(currentMicros - current.previousMicros) >= current.stepDelay){
-      halfStep();
-      current.previousMicros = currentMicros;
-      current.steps--;
+    if (current.dir == 0) {  // Forward
+      stepper.moveTo(current.steps);
+      stepper.setMaxSpeed(current.stepDelay);
+      stepper.setAcceleration(current.stepDelay / 2);
+//      stepper.setSpeed(current.stepDelay);
+    }
+    else {  // Backward...need to provide negative position.
+      stepper.moveTo(0 - current.steps);
+      stepper.setMaxSpeed(current.stepDelay);
+      stepper.setAcceleration(current.stepDelay / 2);
+//      stepper.setSpeed(current.stepDelay);
     }
   }
-  
-  else if(current.state != COMPLETE && current.steps == 0){
+
+  else if (current.state == PROCESSING && stepper.distanceToGo() != 0) {
+//    Serial.print(current.ref);
+//    Serial.print(":dtg:");
+//    Serial.println(stepper.distanceToGo());
+
+    stepper.run();
+//    stepper.runSpeedToPosition();
+  }
+
+  else if (current.state == PROCESSING && stepper.distanceToGo() == 0) {
     Serial.print(current.ref);
     Serial.println(":success");
     current.state = COMPLETE;
+    stepper.setCurrentPosition(0);  // Reset 'current' position so that we don't have to maintain it.
   }
-  
-  else if(!queue.isEmpty()){
+
+  else if (!queue.isEmpty()) {
     current = queue.pop();
   }
-  
-  else if(current.state == COMPLETE && queue.isEmpty()){
-    digitalWrite(SLP, LOW); //SLEEP
+
+  else if (current.state == COMPLETE && queue.isEmpty()) {
+    // TODO:  Does this maintain the stepper position, or does it de-energize the coils, 
+    // allowing the stepper to move freely?
+    sleep();
   }
 }
 
@@ -188,3 +198,4 @@ int freeRam () {
   int v; 
   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
 }
+
